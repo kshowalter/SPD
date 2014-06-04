553cba7f15540628e3c89236d2909f013f6efe1d
Keith Showalter <kshowalter@fsec.ucf.edu>
2014-06-04 15:39:18 -0400

    Moved DOM wrappers to seperate repository. Created local css file. Started to add max/min displays to values.

--- index.js
+++ index.js
@@ -46,254 +46,7 @@ function loadTables(string){
 k.ajax('tables.txt', loadTables);
 
 
-// PV Systems drawing generator
-var elem_prototype = {
 
-    html: function(html){
-       this.elem.innerHTML = html;
-       return this;
-    },
-    append: function(sub_element){
-        this.elem.appendChild(sub_element.elem); 
-        return this;
-    },
-    appendTo: function(parent_element){
-        parent_element.append(this); 
-        return this;
-    },
-    attr: function(attributeName, value ){
-        this.elem[attributeName] = value; 
-        return this;
-    },
-
-
-
-}
-
-var Elem = function(element){
-    var E = Object.create(elem_prototype);
-
-    E.elem = element;
-
-    return E;
-
-}
-
-var $ = function(input){
-    if( typeof input === 'undefined' ) {
-        //log('input needed');
-        return false;
-    }
-    if( input.substr(0,1) === '#' ) {
-        var element = document.getElementById(input.substr(1));
-        return Elem(element);
-    } else if( input.substr(0,1) === '.' ) {
-    
-    } else {
-        if( input === 'value' ) {
-            var element = Value(); 
-            return element;
-        } else if( input === 'selector' ) {
-            var element = Selector(); 
-            return element;
-        } else {
-            var element = document.createElement(input);
-            return Elem(element);
-        }
-    }
-    
-
-
-}
-
-
-var selector_prototype = {
-    change: function(new_value){
-        //log('change to ' + new_value);
-        if( new_value !== undefined ) { 
-            this.set_value(new_value);
-        }
-        this.expanded = !this.expanded;
-        //this.update();
-        update();
-    },
-    update_options: function(){
-        //TODO: find way to do this other than eval
-        if( this.options_reference !== undefined ) {
-            //log("reference exists: "+ this.options_reference)
-            //log(settings.make)
-            //log(components.modules)
-            eval( 'this.options = ' + this.options_reference + ";" );
-            //log('this.options', this.options)
-        }
-        if( this.options !== undefined ) {
-            this.elem_options.innerHTML = '';
-            this.options.forEach(function(value,id){
-                var o = document.createElement('a')
-                o.href = '#';
-                o.setAttribute('class', 'selector_option');
-                o.innerHTML = value;
-                var that = this;
-                o.addEventListener('click', function(){
-                    that.change(value);
-                }, false);
-                this.elem_options.appendChild(o);
-
-            }, this);
-            if( ! (this.options.indexOf(this.value)+1) ){
-                this.set_value(this.options[0]);
-            }
-            //this.update_elements();
-        }
-        return this;
-    },
-    set_value: function(new_value){
-        if( new_value !== undefined ) {
-            this.value = new_value;
-            this.elem_value = document.createElement('a');
-            this.elem_value.href = '#';
-            this.elem_value.setAttribute('class', 'selector');
-            this.elem_value.innerHTML = this.value;
-            var that = this;
-            this.elem_value.addEventListener('click', function(){
-                that.location = this.getBoundingClientRect();
-                that.change();
-            }, false);
-            //this.elem.appendChild(this.elem_value);
-            //this.update_elements();
-            //update();
-        }
-        settings[this.setting] = this.value;
-        //log('settings', this.setting, settings[this.setting])
-        //log('settings', settings[this.setting])
-        return this;    
-    },
-    set_options: function(options_reference) {
-        this.options_reference = options_reference;
-        //TODO: find way to do this other than eval
-        //eval( 'this.options = ' + this.options_reference );
-        //this.update();
-        return this;
-    },
-    set_setting: function(new_setting){
-        this.setting = new_setting;
-        if( settings[this.setting] !== undefined ) {
-            this.set_value(settings[this.setting]);
-        } else {
-            this.set_value();
-        }
-        //this.update();
-        return this;
-    },
-
-    update: function(){
-        //log('updating: ', this.setting)
-        //log('model', settings.pv_model)
-        this.update_options()
-        this.update_elements();
-        return this;
-    },
-    update_elements: function() {
-        if(this.expanded){
-            //log('open')
-            this.elem.innerHTML = "";
-            this.elem.appendChild(this.elem_options);
-        } else {
-            //log('close')
-            this.elem.innerHTML = "";
-            this.elem.appendChild(this.elem_value);
-        }
-    }
-}
-for( var id in elem_prototype ) {
-    if( elem_prototype.hasOwnProperty(id) ) {
-        selector_prototype[id] = elem_prototype[id]; 
-    }
-}
-
-var Selector = function(){
-    var s = Object.create(selector_prototype);
-    //s.options = ['none'];
-    s.expanded = false;
-    //s.setting = setting;
-    s.elem = document.createElement('span');
-    s.elem.setAttribute('class', 'selector_menu');
-
-    s.elem_options = document.createElement('span');
-    s.elem_value = document.createElement('span');
-    s.elem_value.innerHTML = '-';
-    //s.update_options();
-    
-    settings_registry.push(s);
-    return s;
-};
-
-
-
-
-
-var value_prototype = {
-    update: function(){
-        update_system();
-        //log('updating value: ' , this.reference , eval( this.reference))
-        if( this.reference ){
-            eval( 'this.value = ' + this.reference + ';' );
-        }    
-        if( isNaN(Number(this.value)) ){
-            this.elem.innerHTML = this.value;
-        } else {
-            this.elem.innerHTML = Number(this.value).toFixed(3);
-        }
-        return this;
-    },
-    set: function(new_value) {
-        if( typeof new_value !== 'undefined' ){
-            this.value = new_value;
-        }
-        return this;
-    },
-    setRef: function(reference){
-        if( typeof reference !== 'undefined' ){
-            this.reference = reference;
-        }
-        return this;
-    },
-}
-for( var id in elem_prototype ) {
-    if( elem_prototype.hasOwnProperty(id) ) {
-        value_prototype[id] = elem_prototype[id]; 
-    }
-}
-
-function Value() {
-    var v = Object.create(value_prototype);
-    v.elem = document.createElement('span');
-
-    v.value = '-';
-    v.innerHTML = v.value;
-    v.reference = false;
-
-
-    v.update();
-
-    settings_registry.push(v);
-    return v;
-}
-
-var appendElement = function(parentElement,name,attrs,text){
-  var doc = parentElement.ownerDocument;
-  var svg = parentElement;
-  while (svg.tagName!='svg') svg = svg.parentNode;
-  var el = doc.createElementNS(svg.namespaceURI,name);
-  for (var a in attrs){
-    if (!attrs.hasOwnProperty(a)) continue;
-    var p = a.split(':');
-    if (p[1]) el.setAttributeNS(svg.getAttribute('xmlns:'+p[0]),p[1],attrs[a]);
-    else el.setAttribute(a,attrs[a]);
-  }
-  if (text) el.appendChild(doc.createTextNode(text));
-  return parentElement.appendChild(el);
-};
 
 
 
@@ -311,54 +64,13 @@ var appendElement = function(parentElement,name,attrs,text){
 ///////////////////
 // misc functions
 
-function format_floats( elem, index, array ) {
-    array[index] = parseFloat(elem).toFixed(2);
-}
-
-function format_float( str ) {
-    return parseFloat(str).toFixed(2);
-}
-
-var clear = function(div_id){
-    document.getElementById(div_id).innerHTML = '';
-}
-
-function obj_id_array( object ) {
-    if( object !== undefined ) {
-        var a = [];
-        for( var id in object ) {
-            if( object.hasOwnProperty(id) )  {
-                a.push(id);
-            }
-        }
-        return a;
-    }
-}
-
-/*
- *  normRand: returns normally distributed random numbers
- *  http://memory.psych.mun.ca/tech/snippets/random_normal/
- */
-function normRand(mu, sigma) {
-    var x1, x2, rad;
-
-    do {
-        x1 = 2 * Math.random() - 1;
-        x2 = 2 * Math.random() - 1;
-        rad = x1 * x1 + x2 * x2;
-    } while(rad >= 1 || rad === 0);
-
-    var c = Math.sqrt(-2 * Math.log(rad) / rad);
-    var n = x1 * c;
-    return (n * mu) + sigma;
-}
-
-var settings_registry = [];
 
 
 
 ///////////////
 //#system parameters
+
+var settings_registry = [];
 
 var components = {};
 components.inverters = {};
@@ -500,7 +212,13 @@ var AC_types = {
 
 
 
-
+function getSetting(reference){
+    var chain = reference.split('.');
+    var output = settings;
+    chain.forEach( function(name){
+        output = output[name]
+    })
+}
 
 
 
@@ -1845,12 +1563,12 @@ window.onload = function() {
     //*/
 
     $('span').html('Module make: ').appendTo(system_container);
-    $('selector').set_options( 'obj_id_array(components.modules)' ).set_setting('pv_make').update().appendTo(system_container);
+    $('selector').set_options( 'k.obj_id_array(components.modules)' ).set_setting('pv_make').update().appendTo(system_container);
     
     log(settings.pv_make)
     $('br').appendTo(system_container);
     $('span').html('Module model: ').appendTo(system_container);
-    $('selector').set_options( 'obj_id_array(components.modules[settings.pv_make])' ).set_setting('pv_model').update().appendTo(system_container);
+    $('selector').set_options( 'k.obj_id_array(components.modules[settings.pv_make])' ).set_setting('pv_model').update().appendTo(system_container);
 
     $('br').appendTo(system_container);
     $('span').html('Pmax: ').appendTo(system_container);
