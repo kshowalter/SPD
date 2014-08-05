80aa4479221c1730e76fe1889a24e9315127b25f
Keith Showalter <kshowalter@fsec.ucf.edu>
2014-08-05 15:48:19 -0400

    Built latest version.

--- index.js
+++ index.js
@@ -1259,7 +1259,8 @@ var settings = {};
 
 
 settings.system = {};
-settings.registry = [];
+settings.select_registry = [];
+settings.value_registry = [];
 
 var system = settings.system;
 system.wire_config_num = 5;
@@ -2516,7 +2517,7 @@ var selector_prototype = {
     },
     set_value: function(new_value){
         if( new_value !== undefined ) {
-            log( 'new value',this.kontainer.refString,  new_value)
+            //log( 'new value',this.kontainer.refString,  new_value)
             this.kontainer.set(new_value);
         }
         this.elem_value = document.createElement('a');
@@ -2976,28 +2977,23 @@ function showLocation(location_json){
 function update(){
     log('updating');
 
-    // Make sure selectors and value displays are updated
+    update_system(settings);
+
+    settings.select_registry.forEach(function(item){
+        item.update(); 
+    });
 
     update_system(settings);
-    // delete all elements of drawing 
-    //clear_drawing();
 
-    // Recalculate system specs
-    //settings = update_system(settings);
-    
-    // Recalculate drawing related variables
-    //update_values();
+    settings.value_registry.forEach(function(item){
+        item.update(); 
+    });
 
-    // Generate new drawing elements
+    // Make drawing
     settings.elements = mk_drawing(settings);
-
     // Add drawing elements to SVG on screen
     display_svg(settings, svg_container);
 
-    settings.registry.forEach(function(item){
-        //log('updating: ', item)
-        item.update(); 
-    });
 }
 
 
@@ -3117,12 +3113,12 @@ var system_container_array = [
     if( kelem.type === 'selector' ){
         kelem.setRefObj(settings);
         kelem.setUpdate(update);
-        settings.registry.push(kelem);
+        settings.select_registry.push(kelem);
         kelem.update(); 
     } else if( kelem.type === 'value' ){
         kelem.setRefObj(settings);
         //kelem.setUpdate(update_system);
-        settings.registry.push(kelem);
+        settings.value_registry.push(kelem);
     }
 });
 
