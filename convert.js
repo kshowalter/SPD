f21bbef9068c0e32bb07f90dc9a52f18f3d70ecc
Keith Showalter <kshowalter@fsec.ucf.edu>
2014-12-02 17:20:24 -0500

    Setting.json now converted with a try/catch because some things may not be defined the first time around.

--- convert.js
+++ convert.js
@@ -8,6 +8,7 @@ var output = [];
 
 var reg = /\/|\ /;
 
+
 function convert( obj, assign, output){
     for( var name in obj){
         var output_value;
@@ -16,34 +17,54 @@ function convert( obj, assign, output){
         if( ! name.match(reg) && ! name[0].match(/\d/) ) new_assign = assign +"."+ name;
         else new_assign = assign + '["' + name + '"]';
 
+        var line;
+
         if( item instanceof Array ) {
             //output_value = "["+ item.toString() +"]";
             output_value = JSON.stringify(item);
-            output.push( new_assign +" = "+ output_value + ";" );
+            line = new_assign +" = "+ output_value;
         }
         else if( item === null || item === undefined ){
             output_value = item;
-            output.push( new_assign +" = "+ output_value + ";" );
+            line = new_assign +" = "+ output_value;
         }
         else if( typeof item === 'object' ) {
-            output.push( new_assign + " = " + new_assign + " || {};" );
-            output = convert( item, new_assign, output );
+            line = new_assign + " = " + new_assign + " || {}";
         }
         else {
             output_value = item;
-            output.push( new_assign +" = "+ output_value + ";" );
+            line = new_assign +" = "+ output_value;
         }
 
+        line = "    try { " + line + ";}\n    catch(e) { if(settings.state.database_loaded) console.log(e); }";
+
+        output.push(line);
+
+        if( !(item instanceof Array) && typeof item === 'object' ) {
+            output = convert( item, new_assign, output );
+        }
     }
     //console.log(output[]);
     return output;
 }
 
-var output = ['e = {};'];
-for( var name in json ){
-    var item = json[name];
-    output.push( 'e.' + name + ' = function(settings){' );
-    output = convert( item, '    settings', output );
+/*
+var output = ['function check(e){'];
+var output = ['    '];
+var output = ['}'];
+//*/
+
+
+var output = [];
+//output.push('function test(e){ if(settings.state.database_loaded) console.log(e); };');
+output.push('e = {};');
+
+for( var section_name in json ){
+    var item = json[section_name];
+    output.push( 'e.' + section_name + ' = function(settings){' );
+    output.push( "    " + 'settings.'+section_name + " = " + 'settings.'+section_name + " || {};" );
+    output = convert( item, 'settings.'+section_name, output );
+    output.push( '    return settings;' );
     output.push( '};' );
 
 }
