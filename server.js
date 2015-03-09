f81af6787ef695da65b9ba69cb44f13f059b8c8d
Keith Showalter <kshowalter@fsec.ucf.edu>
2015-03-09 15:07:24 -0400

    Setup Express server that takes user input data from the webapp.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 'use strict';
 
 
+
 // Setup
     // Load and create main settings, and save them to the root global object.
-    //g = require('./modules/settings');
+    var g = require('./modules/settings');
     //console.log('settings', g);
 
 
@@ -13,81 +14,79 @@
 
 
 
-var sys = require("sys"),
-my_http = require("http"),
-path = require("path"),
-url = require("url"),
-filesys = require("fs");
-my_http.createServer(function(request,response){
-    var my_path = url.parse(request.url).pathname;
-    var full_path = path.join(process.cwd(),my_path);
-    path.exists(full_path,function(exists){
-        if(!exists){
-            response.writeHeader(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-        }
-        else{
-            filesys.readFile(full_path, "binary", function(err, file) {
-                 if(err) {
-                     response.writeHeader(500, {"Content-Type": "text/plain"});
-                     response.write(err + "\n");
-                     response.end();
+// Server setup
 
-                 }
-                 else{
-                    response.writeHeader(200);
-                    response.write(file, "binary");
-                    response.end();
-                }
+    var bodyParser = require('body-parser');
+    var express = require('express');
 
-            });
-        }
+    var app = express();
+
+    var server = app.listen(4233, function () {
+
+        var host = server.address().address;
+        var port = server.address().port;
+
+        console.log('Example app listening at http://%s:%s', host, port);
+
     });
-}).listen(8080);  
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+    app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+    app.get('/', function (req, res) {
+        res.send('Hello Drawing!');
+    });
 
 
+// Plans machine function
+    app.post('/plans_machine', function(req, res, next) {
+
+        console.log('/ plans machine');
 
 
+        console.log( 'body', req.body );
+        for( var name in req.body ){
+                console.log(name);
+        }
 
-router.post('/', function(req, res, next) {
-
-    console.log('/ plans machine');
-
-
-    //console.log( req.body.user_input_json );
-    var user_input =  JSON.parse( req.body.user_input_json );
-    console.log('user_input', user_input);
-    console.log('address', user_input.location.address);
-    console.log('- plans machine');
-    //for( var name in user_input){
-    //    console.log( name );
-    //}
-    /*
-    // parse URL
-    var url_parts = url.parse(req.url);
-    // parse query
-    var raw = querystring.parse(url_parts.query);
-    // some juggling e.g. for data from jQuery ajax() calls.
-    console.log( 'raw', raw );
-    var data = raw ? raw : {};
-    data = raw.data ? JSON.parse(raw.data) : data;
+        var user_input =  JSON.parse( req.body.user_input_json );
+        console.log('user_input', user_input);
+        console.log('address', user_input.location.address);
+        console.log('- plans machine');
+        //for( var name in user_input){
+        //    console.log( name );
+        //}
+        /*
+        // parse URL
+        var url_parts = url.parse(req.url);
+        // parse query
+        var raw = querystring.parse(url_parts.query);
+        // some juggling e.g. for data from jQuery ajax() calls.
+        console.log( 'raw', raw );
+        var data = raw ? raw : {};
+        data = raw.data ? JSON.parse(raw.data) : data;
 
 
-    console.log( data );
-    //console.log( data.user_input_json );
-    //var user_input = JSON.parse(data.user_input_json);
+        console.log( data );
+        //console.log( data.user_input_json );
+        //var user_input = JSON.parse(data.user_input_json);
 
-    //*/
+        //*/
 
-    //console.log( user_input );
+        //console.log( user_input );
 
-    console.log('- plans machine');
-    //console.log( JSON.parse(req.data) );
-    console.log('\\ plans machine');
+        console.log('- plans machine');
+        //console.log( JSON.parse(req.data) );
+        console.log('\\ plans machine');
 
 
-    //*/
+        //*/
 
-    res.send('plans machine');
-});
+        res.send('plans machine');
+    });
