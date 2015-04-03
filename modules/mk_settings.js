84d7bd79eb9faa0dc8e6279d4c55c4a4b573d9a9
Keith Showalter <kshowalter@fsec.ucf.edu>
2015-04-03 12:06:14 -0400

    Update sheet module names. Added sheet list.

--- modules/mk_settings.js
+++ modules/mk_settings.js
@@ -199,16 +199,44 @@ var mk_settings = function() {
     f.mk_blocks = require('./mk_blocks');
 
     f.mk_page = {};
-    f.mk_page[1] = require('./mk_page_1');
-    f.mk_page[2] = require('./mk_page_2');
-    f.mk_page[3] = require('./mk_page_3');
-    f.mk_page[4] = require('./mk_page_4');
+    f.mk_page['G-001'] = require('./page/G-001');
+    f.mk_page['S-001'] = require('./page/S-001');
+    f.mk_page['W-001'] = require('./page/W-001');
+    f.mk_page['W-002'] = require('./page/W-002');
 
     f.mk_preview = {};
     f.mk_preview[1] = require('./mk_page_preview_1');
     f.mk_preview[2] = require('./mk_page_preview_2');
 
     f.mk_svg= require('./mk_svg');
+
+
+
+    settings.drawing_settings.sheets = [
+        {
+            num: 'G-001',
+            desc: 'Title Sheet'
+        },
+        {
+            num: 'W-001',
+            desc: 'PV system wiring diagram'
+        },
+        {
+            num: 'W-002',
+            desc: 'PV system specifications'
+        },
+        {
+            num: 'S-001',
+            desc: 'Roof details'
+        },
+        {
+            num: 'X-042',
+            desc: 'L.U.E.'
+        },
+    ];
+
+
+
 
 
     return settings;
