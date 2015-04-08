5d4bd5ade5be949fc64afe7ddeea022bc8eb122c
Keith Showalter <kshowalter@fsec.ucf.edu>
2015-04-08 16:08:32 -0400

    Added image element. Re worked how elements are created. Make blocks into groups.

--- modules/mk_svg.js
+++ modules/mk_svg.js
@@ -13,94 +13,101 @@ var mk_svg = function(drawing_parts, drawing_settings){
     var layer_attr = drawing_settings.layer_attr;
     var fonts = drawing_settings.fonts;
 
-    //var svg_elem = document.getElementById('SvgjsSvg1000')
-    var svg_elem = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-    svg_elem.setAttribute('class','svg_drawing');
-    //svg_elem.setAttribute('width', settings.drawing_settings.size.drawing.w);
-    //svg_elem.setAttribute('height', settings.drawing_settings.size.drawing.h);
+    //var svg_document = document.getElementById('SvgjsSvg1000')
+    var svg_document = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+    svg_document.setAttribute('class','svg_drawing');
+
+
+
+    //svg_document.setAttribute('width', settings.drawing_settings.size.drawing.w);
+    //svg_document.setAttribute('height', settings.drawing_settings.size.drawing.h);
     var view_box = '0 0 ' + drawing_size.w + ' ' + drawing_size.h + ' ';
-    svg_elem.setAttribute('viewBox', view_box);
-    //var svg = snapsvg(svg_elem).size(size.drawing.w, size.drawing.h);
+    svg_document.setAttribute('viewBox', view_box);
+    svg_document.setAttribute('xmlns','http://www.w3.org/2000/svg');
+    svg_document.setAttribute('xmlns:xlink','http://www.w3.org/1999/xlink');
+    //var svg = snapsvg(svg_document).size(size.drawing.w, size.drawing.h);
     //var svg = snapsvg('#svg_drawing');
 
     // Loop through all the drawing contents, call the function below.
-    drawing_parts.forEach( function(elem,id) {
-        show_elem_array(elem);
+    drawing_parts.forEach( function(item,id) {
+        svg_document.appendChild(
+            mk_svg_elem(item)
+        );
     });
 
-    function show_elem_array(elem, offset){
-        offset = offset || {x:0,y:0};
+    function mk_svg_elem(item){
         var x,y,attr_name;
-        if( typeof elem.x !== 'undefined' ) { x = elem.x + offset.x; }
-        if( typeof elem.y !== 'undefined' ) { y = elem.y + offset.y; }
+        if( typeof item.x !== 'undefined' ) { x = item.x; }
+        if( typeof item.y !== 'undefined' ) { y = item.y; }
 
-        var attrs = layer_attr[elem.layer_name];
-        if( elem.attrs !== undefined){
-            for( attr_name in elem.attrs ){
-                attrs[attr_name] = elem.attrs[attr_name];
+        var attrs = layer_attr[item.layer_name];
+        if( item.attrs !== undefined){
+            for( attr_name in item.attrs ){
+                attrs[attr_name] = item.attrs[attr_name];
             }
         }
+        var svg_elem;
 
-        if( elem.type === 'rect') {
-            //svg.rect( elem.w, elem.h ).move( x-elem.w/2, y-elem.h/2 ).attr( layer_attr[elem.layer_name] );
+        if( item.type === 'rect') {
+            //svg.rect( item.w, item.h ).move( x-item.w/2, y-item.h/2 ).attr( layer_attr[item.layer_name] );
             //console.log('elem:', elem );
-            //if( isNaN(elem.w) ) {
+            //if( isNaN(item.w) ) {
             //    console.log('error: elem not fully defined', elem)
-            //    elem.w = 10;
+            //    item.w = 10;
             //}
-            //if( isNaN(elem.h) ) {
+            //if( isNaN(item.h) ) {
             //    console.log('error: elem not fully defined', elem)
-            //    elem.h = 10;
+            //    item.h = 10;
             //}
-            var r = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-            r.setAttribute('width', elem.w);
-            r.setAttribute('height', elem.h);
-            r.setAttribute('x', x-elem.w/2);
-            r.setAttribute('y', y-elem.h/2);
-            //console.log(elem.layer_name);
+            svg_elem = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+            svg_elem.setAttribute('width', item.w);
+            svg_elem.setAttribute('height', item.h);
+            svg_elem.setAttribute('x', x-item.w/2);
+            svg_elem.setAttribute('y', y-item.h/2);
+            //console.log(item.layer_name);
             for( attr_name in attrs ){
-                r.setAttribute(attr_name, attrs[attr_name]);
+                svg_elem.setAttribute(attr_name, attrs[attr_name]);
             }
-            svg_elem.appendChild(r);
-        } else if( elem.type === 'line') {
+
+        } else if( item.type === 'line') {
             var points2 = [];
-            elem.points.forEach( function(point){
+            item.points.forEach( function(point){
                 if( ! isNaN(point[0]) && ! isNaN(point[1]) ){
-                    points2.push([ point[0]+offset.x, point[1]+offset.y ]);
+                    points2.push([ point[0], point[1] ]);
                 } else {
-                    console.log('error: elem not fully defined', elem);
+                    console.log('error: elem not fully defined', item);
                 }
             });
-            //svg.polyline( points2 ).attr( layer_attr[elem.layer_name] );
+            //svg.polyline( points2 ).attr( layer_attr[item.layer_name] );
 
-            var l = document.createElementNS("http://www.w3.org/2000/svg", 'polyline');
-            l.setAttribute( 'points', points2.join(' ') );
+            svg_elem = document.createElementNS("http://www.w3.org/2000/svg", 'polyline');
+            svg_elem.setAttribute( 'points', points2.join(' ') );
             for( attr_name in attrs ){
-                l.setAttribute(attr_name, attrs[attr_name]);
+                svg_elem.setAttribute(attr_name, attrs[attr_name]);
             }
-            svg_elem.appendChild(l);
-        } else if( elem.type === 'poly') {
+
+        } else if( item.type === 'poly') {
             var points2 = [];
-            elem.points.forEach( function(point){
+            item.points.forEach( function(point){
                 if( ! isNaN(point[0]) && ! isNaN(point[1]) ){
-                    points2.push([ point[0]+offset.x, point[1]+offset.y ]);
+                    points2.push([ point[0], point[1] ]);
                 } else {
-                    console.log('error: elem not fully defined', elem);
+                    console.log('error: elem not fully defined', item);
                 }
             });
-            //svg.polyline( points2 ).attr( layer_attr[elem.layer_name] );
+            //svg.polyline( points2 ).attr( layer_attr[item.layer_name] );
 
-            var l = document.createElementNS("http://www.w3.org/2000/svg", 'polyline');
-            l.setAttribute( 'points', points2.join(' ') );
+            svg_elem = document.createElementNS("http://www.w3.org/2000/svg", 'polyline');
+            svg_elem.setAttribute( 'points', points2.join(' ') );
             for( attr_name in attrs ){
-                l.setAttribute(attr_name, attrs[attr_name]);
+                svg_elem.setAttribute(attr_name, attrs[attr_name]);
             }
-            svg_elem.appendChild(l);
-        } else if( elem.type === 'text') {
-            //var t = svg.text( elem.strings ).move( elem.points[0][0], elem.points[0][1] ).attr( layer_attr[elem.layer_name] )
+
+        } else if( item.type === 'text') {
+            //var t = svg.text( item.strings ).move( item.points[0][0], item.points[0][1] ).attr( layer_attr[item.layer_name] )
             var font;
-            if( elem.font && fonts[elem.font] ){
-                font = fonts[elem.font];
+            if( item.font && fonts[item.font] ){
+                font = fonts[item.font];
             } else if(fonts[attrs.font]){
                 font = fonts[attrs.font];
             } else {
@@ -110,72 +117,100 @@ var mk_svg = function(drawing_parts, drawing_settings){
                 console.log('Font not found', font, fonts['base']['font-size']);
 
             }
-            var t = document.createElementNS("http://www.w3.org/2000/svg", 'text');
-            if(elem.rotated){
-                //t.setAttribute('transform', "rotate(" + elem.rotated + " " + x + " " + y + ")" );
-                t.setAttribute('transform', "rotate(" + elem.rotated + " " + x + " " + y + ")" );
+
+            svg_elem = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+            if(item.rotated){
+                //t.setAttribute('transform', "rotate(" + item.rotated + " " + x + " " + y + ")" );
+                svg_elem.setAttribute('transform', "rotate(" + item.rotated + " " + x + " " + y + ")" );
             } else {
                 //if( font['text-anchor'] === 'middle' ) y += font['font-size']*1/3;
                 y += font['font-size']*1/3;
             }
             var dy = font['font-size']*1.5;
-            t.setAttribute('x', x);
-            //t.setAttribute('y', y + font['font-size']/2 );
-            t.setAttribute('y', y-dy );
+            svg_elem.setAttribute('x', x);
+            //svg_elem.setAttribute('y', y + font['font-size']/2 );
+            svg_elem.setAttribute('y', y-dy );
 
             for( attr_name in attrs ){
                 if( attr_name === 'stroke' ) {
-                    t.setAttribute( 'fill', attrs[attr_name] );
+                    svg_elem.setAttribute( 'fill', attrs[attr_name] );
                 } else if( attr_name === 'fill' ) {
-                    //t.setAttribute( 'stroke', 'none' );
+                    //svg_elem.setAttribute( 'stroke', 'none' );
                 } else {
-                    t.setAttribute( attr_name, attrs[attr_name] );
+                    svg_elem.setAttribute( attr_name, attrs[attr_name] );
                 }
 
             }
             for( attr_name in font ){
-                t.setAttribute( attr_name, font[attr_name] );
+                svg_elem.setAttribute( attr_name, font[attr_name] );
             }
-            for( attr_name in elem.strings ){
+            for( attr_name in item.strings ){
                 var tspan = document.createElementNS("http://www.w3.org/2000/svg", 'tspan');
                 tspan.setAttribute('dy', dy );
                 tspan.setAttribute('x', x);
-                tspan.innerHTML = elem.strings[attr_name];
-                t.appendChild(tspan);
+                tspan.innerHTML = item.strings[attr_name];
+                svg_elem.appendChild(tspan);
             }
-            svg_elem.appendChild(t);
-        } else if( elem.type === 'circ') {
-            var c = document.createElementNS("http://www.w3.org/2000/svg", 'ellipse');
-            c.setAttribute('rx', elem.d/2);
-            c.setAttribute('ry', elem.d/2);
-            c.setAttribute('cx', x);
-            c.setAttribute('cy', y);
+
+        } else if( item.type === 'circ') {
+            var svg_elem = document.createElementNS("http://www.w3.org/2000/svg", 'ellipse');
+            svg_elem.setAttribute('rx', item.d/2);
+            svg_elem.setAttribute('ry', item.d/2);
+            svg_elem.setAttribute('cx', x);
+            svg_elem.setAttribute('cy', y);
             for( attr_name in attrs ){
-                c.setAttribute(attr_name, attrs[attr_name]);
+                svg_elem.setAttribute(attr_name, attrs[attr_name]);
             }
-            svg_elem.appendChild(c);
             /*
-            c.attributes( layer_attr[elem.layer_name] )
+            c.attributes( layer_attr[item.layer_name] )
             c.attributes({
                 rx: 5,
                 --------------------------
                 ry: 5,
-                cx: elem.points[0][0]-elem.d/2,
-                cy: elem.points[0][1]-elem.d/2
+                cx: item.points[0][0]-item.d/2,
+                cy: item.points[0][1]-item.d/2
             })
-            var c2 = svg.ellipse( elem.r, elem.r )
-            c2.move( elem.points[0][0]-elem.d/2, elem.points[0][1]-elem.d/2 )
+            var c2 = svg.ellipse( item.r, item.r )
+            c2.move( item.points[0][0]-item.d/2, item.points[0][1]-item.d/2 )
             c2.attr({rx:5, ry:5})
-            c2.attr( layer_attr[elem.layer_name] )
-            */
-        } else if(elem.type === 'block') {
+            c2.attr( layer_attr[item.layer_name] )
+            //*/
+        } else if( item.type === 'image') {
+
+            svg_elem = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+            var image = document.createElementNS("http://www.w3.org/2000/svg", 'image');
+            image.setAttribute('x', x);
+            image.setAttribute('y', y);
+            image.setAttribute('width', item.w);
+            image.setAttribute('height', item.h);
+            image.setAttribute('xlink:href', item.href);
+            for( attr_name in attrs ){
+                image.setAttribute(attr_name, attrs[attr_name]);
+            }
+            //g.appendChild(svg_elem);
+            //svg_elem.appendChild(g);
+            svg_elem.innerHTML += image.outerHTML;
+
+
+
+
+        } else if(item.type === 'block') {
             // if it is a block, run this function through each element.
-            elem.drawing_parts.forEach( function(block_elem,id){
-                show_elem_array(block_elem, {x:x, y:y});
+
+            svg_elem = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+            svg_elem.setAttribute('transform', 'translate('+x+','+y+')');
+            item.drawing_parts.forEach( function(block_item,id){
+                svg_elem.appendChild(
+                    mk_svg_elem(block_item)
+                );
             });
         }
+
+        return svg_elem;
+
+
     }
-    return svg_elem;
+    return svg_document;
 };
 
 
