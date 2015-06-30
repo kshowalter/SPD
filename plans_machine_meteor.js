60dcee86b4bc0f9df8aee0ed8e8191eb6a7c4c8c
Keith Showalter <kshowalter@fsec.ucf.edu>
2015-06-30 09:21:30 -0400

    Misc work on converting to Meteor.

--- plans_machine_meteor.js
+++ plans_machine_meteor.js
@@ -13,22 +13,42 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  f.process(settings);
+
+
+
+  var update = function(){
+    console.log('updateing');
+    f.process(settings);
+    update_drawing();
+
+
+    //$('#drawing').empty();
+    //settings.drawing.svgs.forEach(function(svg){
+    //  $('#drawing')
+    //  //.append($('<p>Page '+p+'</p>'))
+    //    .append($(svg))
+    //    .append($('</br>'))
+    //    .append($('</br>'));
+
+    //});
+
+  };
 
   Values.find({type:"user"}).observe({
-    changed: function(dic){
+    changed: function(doc){
       console.log("something changed, recalculating");
-      f.process(settings);
-      $('#drawing').empty();
-      settings.drawing.svgs.forEach(function(svg){
-        $('#drawing')
-        //.append($('<p>Page '+p+'</p>'))
-          .append($(svg))
-          .append($('</br>'))
-          .append($('</br>'));
-
-      });
+      update();
     },
+  });
+
+  Settings.find({id:'process'}).observe({
+    changed: function(doc){
+      if(doc.value){
+        console.log("setting change to 'process' triggered a process");
+        f.process(settings);
+        setSetting('process', false);
+      }
+    }
   });
 
   var version_string = 'Preview'+moment().format('YYYYMMDD');
@@ -41,6 +61,8 @@ if (Meteor.isClient) {
 
 
 
+  console.log("first process");
+  update();
 
   console.log(settings.input);
 
@@ -50,7 +72,7 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
 
-
+/*
     if(dev) {
       for( var section_name in settings.user_input ){
         for( var value_name in settings.user_input[section_name] ){
@@ -60,20 +82,8 @@ if (Meteor.isServer) {
         }
       }
     }
+//*/
 
-    Values.find({ section_name:"module", value_name:"make" }).observe({
-      changed: function(doc){
-        console.log('new value for module make: ', doc);
-        var new_options = _.uniq(Components
-          .find({
-            type:"modules",
-            make:getValue('module', 'make')
-          }).map(function(doc){return doc.model;}
-        ));
-        setOptions("module", "model", new_options );
-      },
-      //setValue = function(section_name, value_name, new_value);
-    });
 
     f.process(settings);
 
@@ -85,20 +95,16 @@ if (Meteor.isServer) {
     });
 
 
+    var server_ready = true;
+
+    Meteor.methods({
+      server_ready: function(){
+        return server_ready;
+      }
+    });
 
   });
 
-  /*
-  Meteor.publish('modules', function() {
-    console.log('publishing modules collection');
-    return Files.find({});
-  });
-
-  Meteor.publish('NEC_tables', function() {
-    console.log('publishing NEC_tables collection');
-    return Files.find({});
-  });
-  //*/
 
 }
 
