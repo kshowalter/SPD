1eaa2888c8b5cdef7a350c8fab106bc2ab704c46
LegendaryTom <>
2015-10-02 13:45:15 -0400

    Added node-oracledb module, this will load the modules/inverters data when the meteor server starts. Follow the instructions in \\shares.fsec.ucf.edu\public\SSR\Projects\SPD\Code\OracleClient\ to install node-oracledb.

--- server/lib/data.js
+++ server/lib/data.js
@@ -1,35 +1,206 @@
+//data = {
+//	oracleConnection: null,
+//
+//	oracleQuery: function(query, /* Optional */ binds, callback) {
+//		/* When binds isn't passed. */
+//		if (arguments.length < 3) data._oracleQuery(query, [], binds);
+//		data._oracleQuery(query, binds, callback);
+//	},
+//
+//	_oracleQuery: function(query, binds, _callback) {
+//
+//		console.log(1, _callback);
+//		var cb = function(err, result) {
+//			if (err) console.error(err.message);
+//			console.log(2, _callback);
+//			_callback(err, result);
+//		};
+//
+//		data.oracleConnection.execute(query, binds, cb);
+//
+//	},
+//	getComponents: function(callback) {
+//		data.oracleQuery("select * from plans_machine.modules2", function(err, modules) {
+//			data.oracleQuery("select * from plans_machine.inverters2", function(err, inverters) {
+//				console.log("getComponents(%s, %s)", modules.rows.length, inverters.rows.length);
+//				callback(err, {modules: modules.rows, inverters: inverters.rows});
+//			});
+//		});
+//	},
+//	getTables: function() {
+//		return JSON.parse(Assets.getText('data/tables.json'));
+//	}
+//}
+
+
+
+
+
+
+
+
+//load_data = function(){
+//
+//
+//
+//	//settings.config_options.NEC_tables = JSON.parse(Assets.getText('data/tables.json'));
+//
+//
+//
+//
+//	////FSEC_database = f.lowercase_properties(FSEC_database);
+//	//
+//	////NEC_tables.remove({});
+//	//
+//	//
+//	//
+//	//var oracledb =  Meteor.npmRequire('oracledb');
+//	//oracledb.outFormat = oracledb.OBJECT;
+//	//oracledb.getConnection(
+//	//{
+//	//	user          : "spd",
+//	//	password      : "inb444cert",
+//	//	connectString : "oraquest.fsec.ucf.edu:1530/FS10QP"
+//	//},
+//	//Meteor.bindEnvironment(function(err, connection)
+//	//{
+//	//	if (err) { console.error(err.message); return; }
+//	//
+//	//	connection.execute(
+//	//	"select * from plans_machine.modules2",
+//	//	[],  // no bind variables
+//	//	Meteor.bindEnvironment(function(err, modules)
+//	//	{
+//	//		if (err) { console.error(err.message); return; }
+//	//
+//	//		connection.execute(
+//	//		"select * from plans_machine.inverters2",
+//	//		[],  // no bind variables
+//	//		Meteor.bindEnvironment(function(err, inverters)
+//	//		{
+//	//			if (err) { console.error(err.message); return; }
+//	//			FSEC_database = { modules: modules.rows, inverters: inverters.rows };
+//	//			settings.components = f.load_database(FSEC_database);
+//	//			FSEC_database = f.lowercase_properties(FSEC_database);
+//	//
+//	//			//console.log("FSEC_database:", FSEC_database);
+//	//			//console.log("FSEC_database2:", FSEC_database2);
+//	//
+//	//			for( var type in FSEC_database ){
+//	//				for( var component_name in FSEC_database[type] ){
+//	//					var component = FSEC_database[type][component_name];
+//	//					component.type = type;
+//	//					PV_Components.upsert(component,component);
+//	//				}
+//	//
+//	//			}
+//	//
+//	//			for( var name in settings.config_options.NEC_tables){
+//	//
+//	//				var table = settings.config_options.NEC_tables[name];
+//	//				NEC_tables.upsert(
+//	//						{name: name},
+//	//						{
+//	//							name: name,
+//	//							table: table,
+//	//						}
+//	//				);
+//	//			}
+//	//
+//	//			settings = mk_inputs(settings);
+//	//
+//	//			Inputs.remove({});
+//	//			for( var section_name in settings.inputs ){
+//	//				for( var value_name in settings.inputs[section_name]){
+//	//					var input = settings.inputs[section_name][value_name];
+//	//					Inputs.insert(input);
+//	//				}
+//	//			}
+//	//
+//	//		}));
+//	//
+//	//	}));
+//	//}));
+//
+//};
+
+
+
 load_data = function(){
 
-  //http://10.173.64.204:8000/temporary/
-  settings.config_options.NEC_tables = JSON.parse(Assets.getText('data/tables.json'));
-  FSEC_database = JSON.parse(Assets.getText('data/fsec_copy.json'));
-
-  settings.components = f.load_database(FSEC_database);
 
 
-  FSEC_database = f.lowercase_properties(FSEC_database);
+	/******************************************************************************************
+	 * Grab Module & Inverter Data
+	 ******************************************************************************************/
+	//FSEC_database = JSON.parse(Assets.getText('data/fsec_copy.json'));
 
-  //NEC_tables.remove({});
+	var oracledb =  Meteor.npmRequire('oracledb');
+	oracledb.outFormat = oracledb.OBJECT;
+	var getConnection = Meteor.wrapAsync(oracledb.getConnection);
+	console.log(getConnection);
+	var connection_string = JSON.parse(Assets.getText('sensitive_data/db_connection.json'));
+	oracledb.getConnection(connection_string, Meteor.bindEnvironment(function(err, connection)
+		{
+			if (err) { console.error(err.message); return; }
 
-  for( var type in FSEC_database ){
-    for( var component_name in FSEC_database[type] ){
-      var component = FSEC_database[type][component_name];
-      component.type = type;
-      PV_Components.upsert(component,component);
-    }
+			connection.execute(
+			"select * from plans_machine.modules2",
+			[],  // no bind variables
+			Meteor.bindEnvironment(function(err, modules)
+			{
+				if (err) { console.error(err.message); return; }
 
-  }
+				connection.execute(
+				"select * from plans_machine.inverters2",
+				[],  // no bind variables
+				Meteor.bindEnvironment(function(err, inverters)
+				{
+					if (err) { console.error(err.message); return; }
+					FSEC_database = { modules: modules.rows, inverters: inverters.rows };
 
-  for( var name in settings.config_options.NEC_tables){
+					console.log("[Loading FSEC_database] modules: %s, inverters: %s", FSEC_database.modules.length, FSEC_database.inverters.length);
 
-    var table = settings.config_options.NEC_tables[name];
-    NEC_tables.upsert(
-      {name: name},
-      {
-        name: name,
-        table: table,
-      }
-    );
-  }
+					settings.config_options.NEC_tables = JSON.parse(Assets.getText('data/tables.json'));
+
+					settings.components = f.load_database(FSEC_database);
+					FSEC_database = f.lowercase_properties(FSEC_database);
+
+					for( var type in FSEC_database )
+					{
+						for( var component_name in FSEC_database[type] ){
+							var component = FSEC_database[type][component_name];
+							component.type = type;
+							PV_Components.upsert(component,component);
+						}
+					}
+
+					for( var name in settings.config_options.NEC_tables){
+
+						var table = settings.config_options.NEC_tables[name];
+						NEC_tables.upsert(
+								{name: name},
+								{
+									name: name,
+									table: table,
+								}
+						);
+					}
+
+					settings = mk_inputs(settings);
+
+					Inputs.remove({});
+					for( var section_name in settings.inputs ){
+						for( var value_name in settings.inputs[section_name]){
+							var input = settings.inputs[section_name][value_name];
+							Inputs.insert(input);
+						}
+					}
+				}));
+
+			}));
+		})
+	);
+
 
 };
--- server/main.js
+++ server/main.js
@@ -7,24 +7,112 @@ document = jsdom();
 wkhtmltopdf = Meteor.npmRequire("wkhtmltopdf");
 PDFMerge = Meteor.npmRequire('pdf-merge');
 
+
 Meteor.startup(function () {
-  // code to run on server at startup
-  Settings.remove({});
-  NEC_tables.remove({});
+	//// code to run on server at startup
+	Settings.remove({});
+	NEC_tables.remove({});
+	PV_Components.remove({});
 
-  PV_Components.remove({});
+	load_data();
+
+	//
+	//data = {
+	//	oracleConnection: null,
+	//
+	//	oracleQuery: function(query, /* Optional */ binds, callback) {
+	//		console.log("oracleQuery(%s, %s, %s)", typeof query, typeof binds, typeof callback);
+	//		/* When binds isn't passed. */
+	//		if (arguments.length < 3) data._oracleQuery(query, [], binds);
+	//		data._oracleQuery(query, binds, callback);
+	//	},
+	//
+	//	_oracleQuery: function(query, binds, callback) {
+	//		console.log("_oracleQuery(%s, %s, %s)", typeof query, typeof binds, typeof callback);
+	//		console.log(1, callback);
+	//		var cb = function(err, result) {
+	//			if (err) console.error(err.message);
+	//			console.log(2, callback);
+	//			if(err) callback(err, result);
+	//			else callback(null, result);
+	//		};
+	//
+	//		data.oracleConnection.execute(query, binds, cb);
+	//
+	//	},
+	//	getComponents: function(callback) {
+	//		data.oracleQuery("select * from plans_machine.modules2", function(err, modules) {
+	//			//data.oracleQuery("select * from plans_machine.inverters2", function(err, inverters) {
+	//				console.log("getComponents(%s, %s)", modules.rows.length/*, inverters.rows.length*/);
+	//				callback(err, {modules: modules.rows/*, inverters.rows.length*/});
+	//			//});
+	//		});
+	//	},
+	//	getTables: function() {
+	//		return JSON.parse(Assets.getText('data/tables.json'));
+	//	}
+	//}
+	//
+	//oracledb = Meteor.npmRequire('oracledb');
+	//
+	//oracledb.outFormat = oracledb.OBJECT;
+	//var connection_info = {
+	//	user          : "spd",
+	//	password      : "inb444cert",
+	//	connectString : "oraquest.fsec.ucf.edu:1530/FS10QP"
+	//};
+	//
+	//oracledb.getConnection(connection_info, function(err, connection){ data.oracleConnection = connection;});
+	//
+	//
+	//Meteor.setTimeout(function(){
+	//	settings.config_options.NEC_tables = data.getTables();
+	//
+	//	for( var name in settings.config_options.NEC_tables){
+	//		var table = settings.config_options.NEC_tables[name];
+	//		NEC_tables.upsert(
+	//				{name: name},
+	//				{
+	//					name: name,
+	//					table: table
+	//				}
+	//		);
+	//	}
+	//
+	//	//FSEC_database = JSON.parse(Assets.getText('data/fsec_copy2.json'));
+	//
+	//	data.getComponents(function(err, components) {
+	//		FSEC_database = components;
+	//		settings.components = f.load_database(FSEC_database);
+	//		FSEC_database = f.lowercase_properties(FSEC_database);
+	//
+	//		//console.log("FSEC_database:", FSEC_database);
+	//
+	//		for( var type in FSEC_database ){
+	//			for( var component_name in FSEC_database[type] ){
+	//				var component = FSEC_database[type][component_name];
+	//				component.type = type;
+	//				PV_Components.upsert(component,component);
+	//			}
+	//		}
+	//
+	//		settings = mk_inputs(settings);
+	//
+	//		Inputs.remove({});
+	//		for( var section_name in settings.inputs ){
+	//			for( var value_name in settings.inputs[section_name]){
+	//				var input = settings.inputs[section_name][value_name];
+	//				Inputs.insert(input);
+	//			}
+	//		}
+	//	});
+	//}, 5000);
+
+});
 
 
-  load_data();
-  settings = mk_inputs(settings);
 
-  Inputs.remove({});
-  for( var section_name in settings.inputs ){
-    for( var value_name in settings.inputs[section_name]){
-      var input = settings.inputs[section_name][value_name];
-      Inputs.insert(input);
-    }
-  }
+	//permit.connectDB();
 
 
   //var jsdom = Meteor.npmRequire('moment');
@@ -58,10 +146,10 @@ Meteor.startup(function () {
   //});
 
 
-  var server_ready = true;
-
-
-
-
-
-});
+//  var server_ready = true;
+//
+//
+//
+//
+//
+//});
