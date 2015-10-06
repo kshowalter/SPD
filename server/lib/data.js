3d8769bcc08ccd305218a5b505c6304a269d644a
LegendaryTom <>
2015-10-06 10:11:17 -0400

    For today's demo, switched back to loading modules & inverters using a static JSON file.

--- server/lib/data.js
+++ server/lib/data.js
@@ -4,17 +4,17 @@
 
 load_data = function(){
 
-	//old_load_data();
-	//return;
+	processData(JSON.parse(Assets.getText('data/fsec_copy.json')), JSON.parse(Assets.getText('data/tables.json')))
+	return;
 
 	/******************************************************************************************
 	 * Grab Module & Inverter Data
 	 ******************************************************************************************/
 
 	var oracledb =  Meteor.npmRequire('oracledb');
-	oracledb.outFormat = oracledb.OBJECT;
-	var getConnection = Meteor.wrapAsync(oracledb.getConnection);
-	console.log(getConnection);
+	oracledb.outFormat = oracledb.OBJECT;  //format of result as an object instead of an array
+
+	// Username, Password, and URL of the database are stored in separate file.
 	var connection_string = JSON.parse(Assets.getText('sensitive_data/db_connection.json'));
 	oracledb.getConnection(connection_string, Meteor.bindEnvironment(function(err, connection)
 		{
@@ -33,45 +33,10 @@ load_data = function(){
 				Meteor.bindEnvironment(function(err, inverters)
 				{
 					if (err) { console.error(err.message); return; }
-					FSEC_database = { modules: modules.rows, inverters: inverters.rows };
+					var FSEC_database = { modules: modules.rows, inverters: inverters.rows };
 
-					console.log("[Loading FSEC_database] modules: %s, inverters: %s", FSEC_database.modules.length, FSEC_database.inverters.length);
+					processData(FSEC_database, JSON.parse(Assets.getText('data/tables.json')))
 
-					settings.config_options.NEC_tables = JSON.parse(Assets.getText('data/tables.json'));
-
-					settings.components = f.load_database(FSEC_database);
-					FSEC_database = f.lowercase_properties(FSEC_database);
-
-					for( var type in FSEC_database )
-					{
-						for( var component_name in FSEC_database[type] ){
-							var component = FSEC_database[type][component_name];
-							component.type = type;
-							PV_Components.upsert(component,component);
-						}
-					}
-
-					for( var name in settings.config_options.NEC_tables){
-
-						var table = settings.config_options.NEC_tables[name];
-						NEC_tables.upsert(
-								{name: name},
-								{
-									name: name,
-									table: table,
-								}
-						);
-					}
-
-					settings = mk_inputs(settings);
-
-					Inputs.remove({});
-					for( var section_name in settings.inputs ){
-						for( var value_name in settings.inputs[section_name]){
-							var input = settings.inputs[section_name][value_name];
-							Inputs.insert(input);
-						}
-					}
 				}));
 
 			}));
@@ -81,10 +46,10 @@ load_data = function(){
 
 };
 
-old_load_data = function() {
+processData = function(FSEC_database, tables) {
 
-	FSEC_database = JSON.parse(Assets.getText('data/fsec_copy.json'));
-	settings.config_options.NEC_tables = JSON.parse(Assets.getText('data/tables.json'));
+	console.log("[Loading FSEC_database] modules: %s, inverters: %s", FSEC_database.modules.length, FSEC_database.inverters.length);
+	settings.config_options.NEC_tables = tables;
 
 	settings.components = f.load_database(FSEC_database);
 	FSEC_database = f.lowercase_properties(FSEC_database);
@@ -119,5 +84,4 @@ old_load_data = function() {
 			Inputs.insert(input);
 		}
 	}
-
-};
+}
