0eac15e3917140c1033da266e729c110ae713644
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-03-15 17:10:23 -0400

    Added report page structure

---
+++ drawing/report/report_page_1.js
@@ -0,0 +1,15 @@
+f.mk_report_page_num[1] = function(settings){
+  var state = settings.state;
+
+  var d = Drawing(settings);
+
+  var size = settings.drawing_settings.size;
+  var loc = settings.drawing_settings.loc;
+
+  var x, y, h, w;
+  d.layer('text');
+
+  var x = size.drawing.w*1/2;
+  var y = 50;
+
+}
