cd8773d09edfa74dc75fa625696d3c63af896efa
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-03-16 14:00:13 -0400

    Finished first test of a report page

--- drawing/report/report_page_2.js
+++ drawing/report/report_page_2.js
@@ -3,13 +3,26 @@ f.mk_report_page_num[2] = function(settings){
 
   var d = Drawing(settings);
 
-  var size = settings.drawing_settings.size;
-  var loc = settings.drawing_settings.loc;
+  var size = settings.report_settings.size;
 
   var x, y, h, w;
+
+  x = size.page.w*1/2;
+  y = 50;
+
   d.layer('text');
 
-  var x = size.sheet.w*1/2;
-  var y = 50;
+  for( var limitation_section_name in settings.info.system_limitations ){
+    var limitation_section = settings.info.system_limitations[limitation_section_name];
+    limitation_section.forEach(function(limitation_text){
+      d.text([x,y], limitation_text, 'text', 'notes');
+      y += 10;
+    });
+
+  }
+
+
+
+  return d;
 
 }
